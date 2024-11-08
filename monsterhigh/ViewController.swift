7ef07ef32ef467937530f6d159fca2c8aeb3f5cc
alumno <alumno@pc-candelap.uacj.mx>
2024-11-08 10:47:02 -0700

    Nuevo

---
+++ monsterhigh/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  monsterhigh
+//
+//  Created by alumno on 11/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
